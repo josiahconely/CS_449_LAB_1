3b62ec83d88ab3e003f5b1c288f617a58d43700a
josiahconely <josiahconely@gmail.com>
2019-02-04 09:45:45 -0600

    button linking

--- app/src/main/java/com/example/lab_1_umpire_buddy/MainActivity.java
+++ app/src/main/java/com/example/lab_1_umpire_buddy/MainActivity.java
@@ -8,38 +8,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button strike_button;
+    private Button ball_button;
+    private TextView strike_text;
+    private TextView ball_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.content_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        strike_button = (Button) findViewById(R.id.strike);
+        ball_button = (Button) findViewById(R.id.ball);
+        strike_text = (TextView) findViewById(R.id.strike_count);
+        ball_text = (TextView) findViewById(R.id.ball_count);
     }
 }
