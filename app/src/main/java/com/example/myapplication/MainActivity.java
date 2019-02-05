79d58f2bfbd5effd251201395513ea7980cede46
josiahconely <josiahconely@gmail.com>
2019-02-04 19:17:50 -0600

    Refactor

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,16 +6,10 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 
-
-
-
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button strike_main_button;
-    private Button ball_main_button;
-    private TextView strike_main_display;
-    private TextView ball_main_display;
+    final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
+    final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
 
     int strike_int = 0;
     int ball_int = 0;
@@ -24,45 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    strike_main_button =  (Button) findViewById(R.id.ball_button);
-    ball_main_button = (Button) findViewById(R.id.ball_button);
-    strike_main_display = (TextView) findViewById(R.id.strike_display);
-    ball_main_display = (TextView) findViewById(R.id.ball_display);
-    }
-
-
-    public void updateBallDisplay(String toThis) {
-        TextView textView = (TextView) findViewById(R.id.ball_display);
-        textView.setText(toThis);
-    }
-    public void updateStrikeDisplay(String toThis) {
-        TextView textView = (TextView) findViewById(R.id.strike_display);
-        textView.setText(toThis);
     }
 
     public void push_ball (android.view.View v){
-        if (ball_int< 3){
-            ball_int++;
-        }
-        else if (ball_int >=3) {
+        ball_int++;
+        if (ball_int >= 3) {
             ball_int = 0;
             strike_int = 0;
         }
-        ball_main_display.setText(String.valueOf(ball_int));
-        strike_main_display.setText(String.valueOf(strike_int));
+        ballCount_display.setText(Integer.toString(ball_int));
+        strikeCount_display.setText(Integer.toString(strike_int));
     }
     public void push_strike (android.view.View v){
-        if (strike_int< 2){
-            strike_int++;
-        }
-        else if (strike_int >=2) {
+        strike_int++;
+        if (strike_int >=2) {
             ball_int = 0;
             strike_int = 0;
         }
-
-        ball_main_display.setText(String.valueOf(ball_int));
-        strike_main_display.setText(String.valueOf(strike_int));
+        ballCount_display.setText(Integer.toString(ball_int));
+        strikeCount_display.setText(Integer.toString(strike_int));
     }
 
 
