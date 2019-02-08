14412fa2832fbc8f2d6e4a19e63c00a5b9af7a72
josiahconely <josiahconely@gmail.com>
2019-02-08 14:55:28 -0600

    Full Functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,24 +18,53 @@ public class MainActivity extends AppCompatActivity {
 
     public void push_ball(View v){
         ball_int++;
-        if (ball_int > 3) {
-            ball_int = 0;
-            strike_int = 0;
-        }
         final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
         ballCount_display.setText(Integer.toString(ball_int));
         final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
         strikeCount_display.setText(Integer.toString(strike_int));
+        if (ball_int > 3) {
+            show_reset(v, false);
+        }
+
     }
     public void push_strike(View v){
         final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
         final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
         strike_int++;
-        if (strike_int >2) {
-            ball_int = 0;
-            strike_int = 0;
-        }
         ballCount_display.setText(Integer.toString(ball_int));
         strikeCount_display.setText(Integer.toString(strike_int));
+        if (strike_int >2) {
+            show_reset(v,true);
+        }
     }
+
+    public void show_reset (View v,boolean is_strike_out){
+        findViewById(R.id.strike_button).setVisibility(View.GONE);
+        findViewById(R.id.ball_button).setVisibility(View.GONE);
+        findViewById(R.id.clear_button).setVisibility(View.VISIBLE);
+        final TextView reset_message = (TextView) findViewById(R.id.reset_message);
+        if (is_strike_out) {
+            reset_message.setText("You're Out!!!");
+        }
+        else {
+            reset_message.setText("Take a Free Walk!!!");
+        }
+        reset_message.setVisibility(View.VISIBLE);
+    }
+
+    public void reset(View v) {
+        ball_int = 0;
+        strike_int = 0;
+        final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
+        ballCount_display.setText(Integer.toString(ball_int));
+        final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
+        strikeCount_display.setText(Integer.toString(strike_int));
+        findViewById(R.id.strike_button).setVisibility(View.VISIBLE);
+        findViewById(R.id.ball_button).setVisibility(View.VISIBLE);
+        findViewById(R.id.clear_button).setVisibility(View.GONE);
+        findViewById(R.id.reset_message).setVisibility(View.GONE);
+
+    }
+
+
 }
