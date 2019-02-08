8ecc99fb96245ad7a5bfbe3f3b28007a8383dd17
josiahconely <josiahconely@gmail.com>
2019-02-08 14:02:05 -0600

    God damn button works

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,11 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
-    final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
-
-    int strike_int = 0;
-    int ball_int = 0;
+    static int strike_int = 0;
+    static int ball_int = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +17,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void push_ball (android.view.View v){
+    public void push_ball(View v){
+
         ball_int++;
-        if (ball_int >= 3) {
+        if (ball_int > 3) {
             ball_int = 0;
             strike_int = 0;
         }
+        final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
         ballCount_display.setText(Integer.toString(ball_int));
+        final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
         strikeCount_display.setText(Integer.toString(strike_int));
     }
-    public void push_strike (android.view.View v){
+    public void push_strike(View v){
+        final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
+        final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
         strike_int++;
-        if (strike_int >=2) {
+        if (strike_int >2) {
             ball_int = 0;
             strike_int = 0;
         }
