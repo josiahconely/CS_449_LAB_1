4c10aa3fe4f4528d24506729e5e79e854f88391c
josiahconely <josiahconely@gmail.com>
2019-02-10 18:55:42 -0600

    Added persistant Memory and Strikeouts

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,8 +14,9 @@ import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    static int strike_int = 0;
-    static int ball_int = 0;
+    static int strike_int;
+    static int ball_int;
+    static int strike_outs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mainToolBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mainToolBar);
+
+        //Sets up persistant variables if they exsist
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        //SharedPreferences.Editor editor = pref.edit();
+        strike_int = pref.getInt("strikes", 0);
+        ball_int = pref.getInt("balls", 0);
+        strike_outs = pref.getInt("strike_outs", 0);
+
+        final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
+        ballCount_display.setText(Integer.toString(ball_int));
+        final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
+        strikeCount_display.setText(Integer.toString(strike_int));
+        final TextView strikeOut_display = (TextView) findViewById(R.id.strike_out_count);
+        strikeOut_display.setText(Integer.toString(strike_outs));
+
     }
 
     @Override
@@ -38,11 +55,21 @@ public class MainActivity extends AppCompatActivity {
 
                 ball_int = 0;
                 strike_int = 0;
+                strike_outs = 0;
+
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putInt("strikes", strike_int);
+                editor.putInt("strike_outs",strike_outs);
+                editor.putInt("balls", ball_int);
+                editor.commit();
 
                 final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
                 ballCount_display.setText(Integer.toString(ball_int));
                 final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
                 strikeCount_display.setText(Integer.toString(strike_int));
+                final TextView strikeOut_display = (TextView) findViewById(R.id.strike_out_count);
+                strikeOut_display.setText(Integer.toString(strike_outs));
                 findViewById(R.id.strike_button).setVisibility(View.VISIBLE);
                 findViewById(R.id.ball_button).setVisibility(View.VISIBLE);
                 findViewById(R.id.clear_button).setVisibility(View.GONE);
@@ -67,17 +94,34 @@ public class MainActivity extends AppCompatActivity {
         if (ball_int > 3) {
             show_reset(v, false);
         }
+
+        //saves for persistance
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("balls", ball_int);
+        editor.commit();
     }
 
     public void push_strike(View v){
-        final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
+
         final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
+
         strike_int++;
-        ballCount_display.setText(Integer.toString(ball_int));
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("strikes", strike_int);
+
         strikeCount_display.setText(Integer.toString(strike_int));
+
+
         if (strike_int >2) {
             show_reset(v,true);
+            strike_outs++;
+            editor.putInt("strike_outs",strike_outs);
+            final TextView strikeOut_display = (TextView) findViewById(R.id.strike_out_count);
+            strikeOut_display.setText(Integer.toString(strike_outs));
         }
+        editor.commit();
     }
 
     public void show_reset (View v,boolean is_strike_out){
@@ -97,6 +141,13 @@ public class MainActivity extends AppCompatActivity {
     public void reset(View v) {
         ball_int = 0;
         strike_int = 0;
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("strikes", strike_int);
+        editor.putInt("balls",ball_int);
+
+
         final TextView ballCount_display = (TextView) findViewById(R.id.ball_display);
         ballCount_display.setText(Integer.toString(ball_int));
         final TextView strikeCount_display = (TextView) findViewById(R.id.strike_display);
@@ -105,5 +156,6 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.ball_button).setVisibility(View.VISIBLE);
         findViewById(R.id.clear_button).setVisibility(View.GONE);
         findViewById(R.id.reset_message).setVisibility(View.GONE);
+        editor.commit();
     }
 }
